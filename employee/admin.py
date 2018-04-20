06932e420df816c315d832d64d098bf9e4a1ed6c
joseteodoro <jteodoro.dev@gmail.com>
2018-04-19 22:22:08 -0300

    adding admin and labels

--- employee/admin.py
+++ employee/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Employee, Department
 
-# Register your models here.
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Employee Info', {'fields': ['name', 'email']}),
+        ('Department', {'fields': ['department']}),
+    ]
+
+    list_display = ('name', 'email', 'department')
+    list_display_links = ('name', 'department',)
+    search_fields = ['name', 'department__name', ]
+
+
+admin.site.register(Department)
--- employee/models.py
+++ employee/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 
+
 class Department(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.name
+        return "Department {}".format(self.name)
+
 
 class Employee(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.EmailField()
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, verbose_name="Employee's name")
+    email = models.EmailField(verbose_name="Email")
+    department = models.ForeignKey(Department, on_delete=models.PROTECT, verbose_name="Department")
+
+    def by_department(self, department):
+        return Employee.objects.filter(department__name=department)
+
+    def by_name(self, name):
+        return Employee.objects.filter(*name)
 
     def __str__(self):
         return "{} of {}".format(self.name, self.department)
