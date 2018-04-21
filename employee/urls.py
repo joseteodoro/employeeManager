beec21e5cb9254401acde86733b73e4734cdd7dd
joseteodoro <jteodoro.dev@gmail.com>
2018-04-21 19:02:19 -0300

    fix urls

--- employee/urls.py
+++ employee/urls.py
@@ -4,15 +4,12 @@ from rest_framework import routers
 from .serializers import DepartmentViewSet, EmployeeViewSet
 from . import views
 
-employeeRouter = routers.DefaultRouter()
-employeeRouter.register(r'^', EmployeeViewSet)
-
-departmentRouter = routers.DefaultRouter()
-departmentRouter.register(r'^', DepartmentViewSet)
+router = routers.DefaultRouter()
+router.register(r'employee', EmployeeViewSet)
+router.register(r'department', DepartmentViewSet)
 
 urlpatterns = [
-    url(r'department', include(departmentRouter.urls)),
-    url(r'employee', include(employeeRouter.urls)),
+    url(r'^', include(router.urls)),
     url(r'^employee/$', views.employee_detail, name='employee'),
     url(r'^employee/(?P<pk>[0-9]+)/$', views.employee_detail,name='employee-detail'),
     url(r'^department/$', views.department_detail, name='department'),
